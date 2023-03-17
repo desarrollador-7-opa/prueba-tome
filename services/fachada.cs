0cf73d77619fb29a916b8613eefe87ac4d840d42
Miguel Angel Diaz D <migerodd888@gmail.com>
2023-03-17 10:37:04 -0500

    cambios a fachada

--- services/fachada.cs
+++ services/fachada.cs
@@ -2,7 +2,18 @@ namespace webapi
 {
     public class Fachada 
     {
-        
+        public Cupon EntregarCupon () 
+        {
+            var entregarCupon = EntregarCupon();
+            var Cupon = entregarCupon();
+            return Cupon;
+        }
+        public int GenerarCompra () 
+        {
+            var entregarCupon = EntregarCupon();
+            var Cupon = entregarCupon();
+            return Cupon;
+        }
     }
 }
 
