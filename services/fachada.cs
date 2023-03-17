6b529d5911353c6b7d4d96673d1430bb28b956bc
Miguel Angel Diaz D <migerodd888@gmail.com>
2023-03-17 11:50:11 -0500

    cambios fachada

--- services/fachada.cs
+++ services/fachada.cs
@@ -1,17 +1,42 @@
 using webapi.Funtions;
 namespace webapi
 {
-    public class Fachada 
+    public class Fachada
     {
-        public string EntregarCupones () 
+        public string FunEntregarCupones()
         {
             var entregarCupon = new EntregarCupon();
             return entregarCupon.EntregarCupones();
         }
-        public string GenerarCompra () 
+        public string FunGenerarCompra()
         {
             var generarCompra = new GenerarCompra();
             return generarCompra.GenerarCompras();
+        }
+        public string FunGenerarCupones()
+        {
+            var generarCupones = new GenerarCupones();
+            return generarCupones.GenerarCuponesMasivos();
+        }
+        public string FunIngresarCliente()
+        {
+            var ingresarCliente = new IngresarCliente();
+            return ingresarCliente.IngresarClientes();
+        }
+        public string FunRedimirCupon()
+        {
+            var redimirCupon = new RedimirCupon();
+            return redimirCupon.RedimirCupones();
+        }
+        public string FunValidarCupon()
+        {
+            var validarCupon = new ValidarCupon();
+            return validarCupon.ValidarCupones();
+        }
+        public string FunValidarExisteCliente()
+        {
+            var validarExisteCliente = new ValidarExisteCliente();
+            return validarExisteCliente.ValidarExisteClientes();
         }
     }
 }
