c976307546125430f9080bc0e7075a2b9e795ae5
Miguel Angel Diaz D <migerodd888@gmail.com>
2023-03-17 12:13:34 -0500

    models

--- Models/TipoProductos.cs
+++ Models/TipoProductos.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace webapi.Models;
@@ -6,7 +7,12 @@ namespace webapi.Models;
 public class TipoProducto
 {
     [Key]
+    [Column("TipoId", TypeName = ("bigint"))]
     public Guid TipoId {get;set;}
-    public string Nombre {get;set;}
-    public string Descripcion {get;set;}
+
+    [Column("Nombre",TypeName  = ("Varchar(100"))]
+    public string Nombre {get;set; } = string.Empty;
+
+    [Column("Descripcion", TypeName = ("Varchar(100"))]
+    public string Descripcion {get;set; } = string.Empty;
 }
