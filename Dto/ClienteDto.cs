5a85967cbe441f5be0d138c69e6cebe6129f0fad
Cristian David Gonzalez Vasquez <directorarquitectura@opa.com.co>
2023-03-17 13:01:08 -0500

    inicio dto

---
+++ Dto/ClienteDto.cs
@@ -0,0 +1,13 @@
+namespace webapi.Dal
+{
+    public class Clientedto
+    {
+        public long Nombre { get; set; }
+
+
+        public long Cedula { get; set; }
+
+
+        public string? Telefono { get; set; }
+    }
+}
---
+++ Dto/CuponDto.cs
@@ -0,0 +1,15 @@
+namespace webapi.Dal
+{
+    public class CuponDto
+    {
+       
+        public int Valor { get; set; }
+        
+        public string Estado { get; set; }
+        
+        public string Entregado { get; set; }
+     
+        public DateTime FechaVence { get; set; }
+    }
+
+}
