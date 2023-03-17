65552ce7dc7d8a20243f422cac3e01a81c51d36f
Cristian David Gonzalez Vasquez <directorarquitectura@opa.com.co>
2023-03-17 10:57:02 -0500

    se corrige namespace

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,13 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
-namespace webapi.Models;
-
-public class Cliente
+namespace webapi.Models
 {
-    [key]
-    public Guid ClienteId {get;set;}
-    public string Nombre {get;set;}
-    public int Cedula {get;set;}
-    public string Telefono {get;set;}
+
+    public class Cliente
+    {
+        [key]
+        public Guid ClienteId { get; set; }
+        public string Nombre { get; set; }
+        public int Cedula { get; set; }
+        public string Telefono { get; set; }
+    }
+
 }
