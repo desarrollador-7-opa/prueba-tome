6e53a7355148c7a3e18493a3e354ef99743cbed6
Miguel Angel Diaz Delgado <migerodd888@gmail.com>
2023-03-21 14:58:16 -0500

    instala libr SQL y Tools y se modifica context

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
-// using Microsoft.AspNetCore.Mvc;
-// using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using prueba_tome;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var conexionString = builder.Configuration.GetConnectionString("cnConexion");
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// builder.Services.AddDbcontext<FachadaContext>(builder.Configuration.GetConnectionString("cnConexion"));
+builder.Services.AddDbContext<FachadaContext>(options=>options.UseSqlServer(conexionString));
 
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
