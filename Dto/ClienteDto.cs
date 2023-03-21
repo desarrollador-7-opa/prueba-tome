731b77d111fb4c2082a9fda855d9e1df41b969fe
Miguel Angel Diaz Delgado <migerodd888@gmail.com>
2023-03-21 12:09:33 -0500

    ordenar dtos

--- Dto/ClienteDto.cs
+++ Dto/ClienteDto.cs
@@ -3,11 +3,7 @@ namespace webapi.Dal
     public class Clientedto
     {
         public long Nombre { get; set; }
-
-
         public long Cedula { get; set; }
-
-
         public string? Telefono { get; set; }
     }
 }
--- Dto/CuponDto.cs
+++ Dto/CuponDto.cs
@@ -2,13 +2,9 @@ namespace webapi.Dal
 {
     public class CuponDto
     {
-       
         public int Valor { get; set; }
-        
         public string Estado { get; set; }
-        
         public string Entregado { get; set; }
-     
         public DateTime FechaVence { get; set; }
     }
 
