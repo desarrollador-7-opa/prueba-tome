bd465ef23d26994344d4a9f5187125194ca1cc93
Victor Taborda <109089841+desarrollador-7-opa@users.noreply.github.com>
2023-03-21 11:17:28 -0500

    Update FachadaContext.cs
    se corrige namespace del objeto

--- FachadaContext.cs
+++ FachadaContext.cs
@@ -1,14 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using webapi.Models;
 
-namespace webapi;
-
-public class FachadaContext: DbContext
+namespace webapi
 {
-    public DbSet<Cliente> Categorias {get;set;}
-    public DbSet<Cupon> Cupones {get;set;}
-    public DbSet<CuponesCliente> CuponesClientes {get;set;}
-    public DbSet<Producto> Productos {get;set;}
-    public DbSet<TipoProducto> TipoProductos {get;set;}
-    public FachadaContext(DbContextOptions<FachadaContext> options) :base(options) { }
+
+    public class FachadaContext : DbContext
+    {
+        public DbSet<Cliente> Categorias { get; set; }
+        public DbSet<Cupon> Cupones { get; set; }
+        public DbSet<CuponesCliente> CuponesClientes { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<TipoProducto> TipoProductos { get; set; }
+        public FachadaContext(DbContextOptions<FachadaContext> options) : base(options) { }
+    }
+
 }
