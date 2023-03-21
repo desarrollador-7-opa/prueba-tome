ebbf488e8870308a7f4381d10be3b90a563fe2b2
Miguel Angel Diaz Delgado <migerodd888@gmail.com>
2023-03-21 16:05:10 -0500

    f

--- Program.cs
+++ Program.cs
@@ -8,18 +8,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<FachadaContext>
     (options => options.UseSqlServer(builder.Configuration.GetConnectionString("cnConexion")));
-
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
-
-
-
