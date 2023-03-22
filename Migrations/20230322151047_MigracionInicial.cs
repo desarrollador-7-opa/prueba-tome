1d89a6dc6db86120da1bb4ec522fdca7198f20b2
Miguel Angel Diaz Delgado <migerodd888@gmail.com>
2023-03-22 10:11:23 -0500

    migracion inicial

---
+++ Migrations/20230322151047_MigracionInicial.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace prueba_tome.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    idcliente = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nombreintegrado = table.Column<string>(type: "varchar(100)", nullable: false),
+                    cedula = table.Column<long>(type: "bigint", nullable: false),
+                    telefono = table.Column<string>(type: "varchar(20)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.idcliente);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cupones",
+                columns: table => new
+                {
+                    idcupon = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    valor = table.Column<decimal>(type: "numeric(18,0)", nullable: false),
+                    estado = table.Column<string>(type: "char(1)", nullable: true),
+                    entregado = table.Column<string>(type: "char(1)", nullable: true),
+                    fechavence = table.Column<DateTime>(type: "datetime", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cupones", x => x.idcupon);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CuponesClientes",
+                columns: table => new
+                {
+                    idcuponescliente = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    idcliente = table.Column<long>(type: "bigint", nullable: false),
+                    idcupon = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CuponesClientes", x => x.idcuponescliente);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Valor = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TipoId = table.Column<decimal>(type: "numeric(18,0)", nullable: false),
+                    Cantidad = table.Column<decimal>(type: "numeric(18,0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TipoProductos",
+                columns: table => new
+                {
+                    TipoId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "Varchar(100)", nullable: false),
+                    Descripcion = table.Column<string>(type: "Varchar(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoProductos", x => x.TipoId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Cupones");
+
+            migrationBuilder.DropTable(
+                name: "CuponesClientes");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+
+            migrationBuilder.DropTable(
+                name: "TipoProductos");
+        }
+    }
+}
